0de510e14fe6c6cdf31d48b5a6819de229f2fa3a
Nabi1 <thibaut.cointet@gmail.com>
2024-10-08 12:39:03 +0200

    feat(searchBar): add loop icon

--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { FiSearch } from "react-icons/fi";
 
 type SearchBarProps = {
   onSearch: (query: string) => void;
@@ -15,14 +16,21 @@ export const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="w-full">
+    <form onSubmit={handleSubmit} className="relative w-full">
       <input
         type="text"
         placeholder="Rechercher un film..."
         value={query}
         onChange={(e) => setQuery(e.target.value)}
-        className="w-full p-4 text-lg border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+        className="w-full p-4 pl-12 text-lg border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
       />
+
+      <button
+        type="submit"
+        className="absolute left-4 top-1/2 transform -translate-y-1/2 text-gray-400 focus:outline-none"
+      >
+        <FiSearch size={24} />
+      </button>
     </form>
   );
 };

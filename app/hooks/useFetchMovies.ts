30aeb4f8c6a40fe1d17751eb079e1691d7cb00ef
Nabi1 <thibaut.cointet@gmail.com>
2024-10-08 11:06:07 +0200

    feat(trending): display trending movies by default

--- app/hooks/useFetchMovies.ts
+++ app/hooks/useFetchMovies.ts
@@ -1,16 +1,18 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 
 const useSearchMovies = () => {
   const [movies, setMovies] = useState<any[]>([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
+  const [isSearching, setIsSearching] = useState(false);
 
   const searchMovies = async (query: string) => {
     if (!query) return;
 
     setLoading(true);
     setError(null);
+    setIsSearching(true);
 
     try {
       const response = await axios.get(
@@ -18,11 +20,33 @@ const useSearchMovies = () => {
       );
       setMovies(response.data.results);
     } catch (err) {
-      setError("Error while searching for movies.");
+      setError("Error while fetching movies.");
     } finally {
       setLoading(false);
     }
   };
+
+  const fetchTrendingMovies = async () => {
+    setLoading(true);
+    setError(null);
+
+    try {
+      const response = await axios.get(
+        `https://api.themoviedb.org/3/trending/movie/week?api_key=1645a51e3552af467fa874335328bec9`
+      );
+      setMovies(response.data.results);
+    } catch (err) {
+      setError("Error while fetching trending movies.");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (!isSearching) {
+      fetchTrendingMovies();
+    }
+  }, [isSearching]);
 
   return { movies, searchMovies, loading, error };
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import React from "react";
-
 import { SearchBar } from "./components/SearchBar";
 import { MovieCard } from "./components/MovieCard";
 import useFetchMovies from "./hooks/useFetchMovies";
